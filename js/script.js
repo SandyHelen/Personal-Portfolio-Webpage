5832ab54d9f84167ff54cb924d1d219e1407665e
SandyHelen <sandyhelen@hotmail.com>
2021-03-21 15:38:38 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let nav = document.querySelector('#navbar')
+let buttonMenu = document.querySelector('.botao-menu')
+
+buttonMenu.addEventListener('click', function(){
+    nav.classList.toggle('hide')
+})
+
